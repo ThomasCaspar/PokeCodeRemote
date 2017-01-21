400f2862f188f6cdb53eaacdcc3f11b0b8c4b98e
CASPAR Thomas <CASPAR Thomas>
2017-01-21 20:08:36 +0100

    init

---
+++ app/src/main/java/pokecode/pokecoderemote/MainActivity.java
@@ -0,0 +1,13 @@
+package pokecode.pokecoderemote;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
