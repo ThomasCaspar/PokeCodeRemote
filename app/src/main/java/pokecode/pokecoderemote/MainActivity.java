865b2f7a83013b5d150596de685d83ab78ef74fb
lilysa <lilysa35@gmail.com>
2017-01-22 01:53:14 +0100

    interface avec bluetooth sans images

--- app/src/main/java/pokecode/pokecoderemote/MainActivity.java
+++ app/src/main/java/pokecode/pokecoderemote/MainActivity.java
@@ -1,16 +1,51 @@
 package pokecode.pokecoderemote;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.TextView;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 
-public class MainActivity extends Activity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.Set;
+
+public class MainActivity extends Activity  {
+    Button b1,b2,b3,b4;
+    private BluetoothAdapter BA;
+    private Set<BluetoothDevice>pairedDevices;
+    ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        b3=(Button)findViewById(R.id.buttonList);
+
+        BA = BluetoothAdapter.getDefaultAdapter();
+        lv = (ListView)findViewById(R.id.listView);
+    }
+
+
+
+
+    public void list(View v){
+        pairedDevices = BA.getBondedDevices();
+
+        ArrayList list = new ArrayList();
+
+        for(BluetoothDevice bt : pairedDevices) list.add(bt.getName());
+        Toast.makeText(getApplicationContext(), "Recuperation des appareils apperes",Toast.LENGTH_SHORT).show();
+
+        final ArrayAdapter adapter = new  ArrayAdapter(this,android.R.layout.simple_list_item_1, list);
+
+        lv.setAdapter(adapter);
     }
 }
