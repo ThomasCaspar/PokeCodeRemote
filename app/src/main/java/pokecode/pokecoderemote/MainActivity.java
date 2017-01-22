5628ba3e8c4a439405dbbb00eda534dca95ef2d1
lilysa <lilysa35@gmail.com>
2017-01-22 05:15:42 +0100

    implementation methodes controle de la voiture par l'interface

--- app/src/main/java/pokecode/pokecoderemote/MainActivity.java
+++ app/src/main/java/pokecode/pokecoderemote/MainActivity.java
@@ -3,25 +3,54 @@ package pokecode.pokecoderemote;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-
 import android.widget.Toast;
+import android.os.Handler;
+import android.os.Message;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Set;
 
 public class MainActivity extends Activity  {
     Button haut, bas, freinB, warnB, bl, phares, essuieG, stopB, gauche, droite, clignG, clignD;
+    int vitesse = 1;
+    boolean h = false,b = false,f = false,w = false,p = false,e = false,s = false;
+    int i = 0;
     private BluetoothAdapter BA;
     private Set<BluetoothDevice>pairedDevices;
+    private BtInterface btS = null;
     ListView lv;
     private BluetoothDevice voiture;
+    private long lastTime = 0;
+
+    final Handler handler = new Handler() {
+        public void handleMessage(Message msg) {
+            String data = msg.getData().getString("receivedData");
+
+            long t = System.currentTimeMillis();
+            if(t-lastTime > 100) {// Pour éviter que les messages soit coupés
+
+                lastTime = System.currentTimeMillis();
+            }
+
+        }
+    };
+
+    final Handler handlerStatus = new Handler() {
+        public void handleMessage(Message msg) {
+            int co = msg.arg1;
+            if(co == 1) {
+
+            } else if(co == 2) {
+
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +72,118 @@ public class MainActivity extends Activity  {
 
         BA = BluetoothAdapter.getDefaultAdapter();
         lv = (ListView)findViewById(R.id.listView); //ligne à réécrire pour otus les boutons
+        btS  = new BtInterface(handlerStatus, handler);
+
+        haut.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                btS.sendData("turn 0");
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+               switch (vitesse){
+
+                   case 1:
+                       btS.sendData("drive 1 3333");
+                       vitesse++;
+                       break;
+                   case 2 :
+                       btS.sendData("drive 1 6666");
+                       vitesse++;
+                       break;
+                   case 3:
+                       btS.sendData("drive 1 10000");
+                       break;
+               }
+
+
+            }
+        });
+
+        bas.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                switch (vitesse){
+
+                    case 1:
+                        btS.sendData("drive 2 3333");
+                        break;
+                    case 2 :
+                        btS.sendData("drive 1 3333");
+                        vitesse--;
+                        break;
+                    case 3:
+                        btS.sendData("drive 1 6666");
+                        vitesse--;
+                        break;
+                }
+            }
+        });
+        droite.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                btS.sendData("turn 1");
+            }
+        });
+        gauche.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                btS.sendData("turn 2");
+            }
+        });
+        freinB.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                btS.sendData("drive 0 0");
+                vitesse = 1; //on repartira à la vitesse 1
+            }
+        });
+        warnB.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //A remplir
+            }
+        });
+        phares.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if(p) {
+                    btS.sendData("light 0");
+                    p = false;
+                }else {
+                    btS.sendData("light 1");
+                    p = true;
+                }
+            }
+        });
+        essuieG.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+               if(e) {
+                   btS.sendData("essuieglace 0");
+                   e = false;
+               }else{
+                   btS.sendData("essuieglace 1");
+                   e = true;
+               }
+            }
+        });
+        stopB.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                btS.sendData("stoplight 1");
+            }
+        });
+        clignG.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //A remplir
+            }
+        });
+        clignD.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //A remplir
+            }
+        });
+
+
+
+
+
+
     }
 
     public void vitessePlus(View v){
@@ -95,6 +236,8 @@ public class MainActivity extends Activity  {
             }
 
         }
+        btS.connect();
+
 
     }
 }
